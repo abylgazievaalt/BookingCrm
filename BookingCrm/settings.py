6fb695dcd3e731e4a674f8ef65d8420afb4240a2
Altynai <abylgazievaalt@gmail.com>
2020-11-16 21:08:18 +0600

    added reservation and payment models + channels

--- BookingCrm/settings.py
+++ BookingCrm/settings.py
@@ -105,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+from easy_thumbnails.conf import Settings as thumbnail_settings
+THUMBNAIL_PROCESSORS = (
+    'image_cropping.thumbnail_processors.crop_corners',
+) + thumbnail_settings.THUMBNAIL_PROCESSORS
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
--- webapp/models.py
+++ webapp/models.py
@@ -1,4 +1,7 @@
+import uuid as uuid
+from asgiref.sync import async_to_sync
 from django.db import models
+from channels.layers import get_channel_layer
 
 # Create your models here.
 from django.db.models import Min
@@ -13,6 +16,38 @@ SIDE_CHOICES = (
     ('east', 'Восток'),
 )
 
+CLIENT_TYPE_CHOICES = (
+    ('individual', 'Физ. лицо'),
+    ('entity', 'Организация')
+)
+
+PAYMENT_TYPE_CHOICES = (
+    ('offline', 'офис'),
+    ('online', 'онлайн'),
+)
+
+PAYMENT_STATUS_CHOICES = (
+    ('not paid', 'Не оплачено'),
+    ('fully paid', 'Оплачено полностью'),
+    ('partially paid', 'Оплачено частично'),
+)
+
+RESERVATION_STATUS_CHOICES = (
+    ('active', 'Активный'),
+    ('not paid', 'Клиент не внес оплату'),
+    ('date changed', 'Клиент поменял дату'),
+    ('other', 'Другая причина')
+)
+
+async def send_notification(feedbacks_request):
+    channel_layer = get_channel_layer()
+    await channel_layer.group_send("notification_group", {
+        "type": "notify",
+        "content": {
+                    "feedbacks_request": feedbacks_request,
+                   },
+    })
+
 
 class Discount(models.Model):
     discount = models.IntegerField('Скидка', default=0, null=True)
@@ -23,6 +58,138 @@ class Discount(models.Model):
 
     def __str__(self):
         return '%s%%' %(self.discount) or ''
+
+
+class Meal(models.Model):
+    date = models.DateField('Дата')
+    breakfast = models.BooleanField('Завтрак', default=True)
+    lunch = models.BooleanField('Обед', default=True)
+    dinner = models.BooleanField('Ужин', default=True)
+    reservation = models.ForeignKey('Reservation',on_delete=models.CASCADE, related_name='meals',
+                                    verbose_name='Бронь номера', null=True)
+
+    class Meta:
+        verbose_name = 'Питание'
+        verbose_name_plural = 'Питание'
+
+    def __str__(self):
+        return '%s %s %s' % (self.breakfast, self.lunch, self.dinner)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        reservation = Reservation.objects.get(id=self.reservation.id)
+        reservation.save()
+
+    def delete(self):
+        reservation = Reservation.objects.get(id=self.reservation.id)
+        super(Meal, self).delete()
+        reservation.save()
+
+
+class Reservation(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    client_type = models.CharField('Тип клиента', max_length=10, choices=CLIENT_TYPE_CHOICES, default='individual')
+    room = models.ForeignKey('webapp.Guestroom', on_delete=models.CASCADE, null=True, related_name='reservations', verbose_name='Номер')
+    arrival_date = models.DateField('Дата заезда')
+    departure_date = models.DateField('Дата выезда')
+    count_calendar = models.PositiveSmallIntegerField('Количество людей с питанием', default=0)
+    count_of_people = models.PositiveSmallIntegerField('Общее кол-во людей')
+    name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилия', max_length=100)
+    phone = models.CharField('Номер телефона', max_length=100)
+    email = models.EmailField('Электроная почта', max_length=200, unique=False)
+    comment = models.TextField('Комментарий', default='', blank=True)
+    payment_type = models.CharField(max_length=50, choices=PAYMENT_TYPE_CHOICES, default=PAYMENT_TYPE_CHOICES[0][0],
+                                    verbose_name='Тип оплаты')
+    payment_status = models.CharField(max_length=50, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_CHOICES[0][0],
+                                      verbose_name='Статус оплаты')
+    total_sum = models.PositiveIntegerField('Итоговая сумма', default=0)
+    discount = models.IntegerField('Скидка', default=0, null=True)
+    status = models.CharField(max_length=12, choices=RESERVATION_STATUS_CHOICES, default='active', verbose_name='Статус')
+    payment_url = models.CharField('Ссылка на оплату', max_length=1000, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Бронирование'
+        verbose_name_plural = 'Бронирования'
+
+    def __str__(self):
+        return self.name or ""
+
+    # def get_total_sum(self):
+    #     meals = None
+    #     meals_sum = 0
+    #     sum = 0
+    #
+    #     try:
+    #         meals = Meal.objects.filter(reservation=self)
+    #     except:
+    #         pass
+    #
+    #     arrival = datetime.datetime.strptime(str(self.arrival_date), '%Y-%m-%d')
+    #     departure = datetime.datetime.strptime(str(self.departure_date), '%Y-%m-%d')
+    #     delta_days = (departure - arrival).days
+    #
+    #     try:
+    #         prices = MealPrice.objects.first()
+    #         total_per_day = prices.breakfast + prices.lunch + prices.dinner
+    #         meals_sum =  total_per_day * delta_days * self.count_of_people
+    #
+    #         if meals:
+    #             custom_meals_sum = 0
+    #             for item in self.meals.all():
+    #                 custom_meals_sum += self.count_of_people * item.get_cost_per_person()
+    #
+    #             meals_sum -= meals.count() * total_per_day * self.count_of_people
+    #             meals_sum += custom_meals_sum
+    #     except:
+    #         meals_sum = 0
+    #
+    #     try:
+    #         room_price = Price.objects.get(room_type=self.room.type, roominess=self.room.roominess,
+    #                                         from_date__lte=self.departure_date, to_date__gte=self.arrival_date,
+    #                                         housing=self.room.housing).price
+    #     except:
+    #         room_price = self.room.default_price
+    #
+    #     sum = delta_days * room_price + meals_sum
+    #     if self.discount is not None:
+    #         sum -= sum *(self.discount.discount / 100.0)
+    #     return round(sum)
+
+    def save(self, *args, **kwargs):
+        self.room.status = 'Забронировано'
+        reservation_request = 'Новая бронь'
+        # self.total_sum = self.get_total_sum()
+        super().save(*args, **kwargs)
+        name = f'{self.name[0]}. {self.last_name}'
+        try:
+            Payment.objects.get(reservation=self)
+        except:
+            Payment.objects.create(
+                reservation=self, name=name, total_sum=self.total_sum,
+                payment_type=self.payment_type, status='not paid'
+            )
+        if self.pk is None:
+            async_to_sync(send_notification)(reservation_request)
+
+
+class Payment(models.Model):
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ('-id',)
+
+    reservation = models.OneToOneField(Reservation, verbose_name='Бронь', on_delete=models.CASCADE, related_name='payment')
+    date = models.DateTimeField('Дата', auto_now_add=True, null=True)
+    name = models.CharField('ФИО', max_length=256)
+    total_sum = models.PositiveIntegerField('Итоговая сумма', default=0)
+    payment_type = models.CharField(max_length=50, choices=PAYMENT_TYPE_CHOICES, default=PAYMENT_TYPE_CHOICES[0][0],
+                                    verbose_name='Способ оплаты')
+    status = models.CharField(max_length=50, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_CHOICES[0][0],
+                                    verbose_name='Тип оплаты')
+
+    def __str__(self):
+        return f'{self.name} | {self.total_sum} | {self.date}'
 
 
 class Roominess(models.Model):
