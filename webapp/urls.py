21e7241408137e02a71dd41db5ca6c896e02f407
Eldos Atalov <eldosatalov@gmail.com>
2020-11-19 23:01:30 +0600

    add contacts

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
 
-from webapp.views import IndexView
+from webapp.views import IndexView, RoomListView
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='index'),
+    path('list', RoomListView.as_view(), name='offers')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from webapp.models import Guestroom
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class RoomListView(ListView):
+    model = Guestroom
+    template_name = 'offers.html'
